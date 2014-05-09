8235bcb07acc28276079ee37ad1306deeefbfd71
vindolin <vindolin@gmail.com>
2014-05-09 16:06:38 +0200

    oops wrong url

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from distutils.core import setup
 
 setup(
     name='ninepatch',
-    version='0.1.0',
+    version='0.1.1',
     author='Thomas Schüßler',
     author_email='vindolin@gmail.com',
     packages=['ninepatch', 'ninepatch.test'],
     scripts=['bin/ninepatch'],
-    url='http://pypi.python.org/pypi/TowelStuff/',
+    url='https://github.com/vindolin/ninepatch',
     license='MIT License',
     description='Slice and scale 9-patch images',
     long_description=open('README.txt').read(),
