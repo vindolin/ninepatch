e3044390f24428dd9807a91708504f768f93f537
vindolin <vindolin@gmail.com>
2014-05-10 08:46:53 +0200

    new release

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@ from distutils.core import setup
 
 setup(
     name='ninepatch',
-    version='0.1.3',
+    version='0.1.4',
     author='Thomas Schüßler',
     author_email='vindolin@gmail.com',
     packages=['ninepatch', 'ninepatch.test'],
-    scripts=['bin/ninepatch'],
+    scripts=['bin/ninepatch', 'bin/ninepatch_viewer'],
     url='https://github.com/vindolin/ninepatch',
     license=open('LICENSE').read(),
     description='Slice and scale 9-patch images',
