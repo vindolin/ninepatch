9c3d2902032d4ae10e579395679ec12eb661e4ef
vindolin <vindolin@gmail.com>
2014-09-06 18:18:51 +0200

    version number, removed pypy from travis conf

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='ninepatch',
-    version='0.1.10',
+    version='0.1.11',
     author='Thomas Schüßler',
     author_email='vindolin@gmail.com',
     packages=['ninepatch'],
